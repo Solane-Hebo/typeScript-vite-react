205e4629a67c58d73feeb0ba4f13b494485b2e5c
Meron20 <meronbeyene005@gmail.com>
2025-09-02 21:17:50 +0200

    Added new folder and functions

--- forum/src/app.tsx
+++ forum/src/app.tsx
@@ -5,9 +5,12 @@ import type { QNAThread, Thread } from "./types";
 import { AuthWidget, useSession } from "./auth";
 import ListeRoute from "./routes/ListRoute";
 import ListRoute from "./routes/ListRoute";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 
 export default function App() {
+
   return (
     <BrowserRouter>
       <Shell />
@@ -50,7 +53,15 @@ function Shell() {
       <header className="sticky top-0 z-10 bg-gray-950 border-b">
         <div className="max-w-5xl mx-auto px-4 py-3 flex items-center justify-between gap-4">
           <Link to="/" className="text-2xl font-bold">Forum</Link>
-          <AuthWidget repo={repo} currentUser={currentUser} setCurrentUser={setCurrentUser} />
+
+          {currentUser ? (
+            <span> Inloggad som <b>{currentUser.userName}</b></span>
+          ) : (
+            <div className="flex gap-3">
+              <Link to="/login" className="px-3 py-1.5 border rounded-xl hover:bg-gray-700">Logga in</Link>
+              <Link to="/register" className="px-3 py-1.5 border rounded-xl hover:bg-gray-700">Registrera</Link>
+            </div>
+          )}
         </div>
       </header>
 
@@ -58,6 +69,8 @@ function Shell() {
         <Routes>
            <Route path="/" element={
             <ListRoute threads={repo.state.threads}/> }/>
+            <Route path="/login" element={<Login  repo={repo} setCurrentUser={setCurrentUser}/>} />
+           <Route path="/register" element={<Register repo={repo} setCurrentUser={setCurrentUser}/>} />
         </Routes>
       </main>
     </div>
